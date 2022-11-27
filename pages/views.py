17868e6295dbb793efc887ee03207caaa31006c8
Robert Vari <mail.robertvari@gmail.com>
2022-11-27 16:29:59 +0100

    update

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    return render(request, "home.html")
+    context = {
+        "name": "Robert Vari",
+        "subtitle": "photography"
+    }
+
+    return render(request, "home.html", context)
 
 
 def about_view(request):
@@ -11,3 +16,7 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, "contact.html")
+
+
+def gallery_view(request):
+    return render(request, "gallery.html")
