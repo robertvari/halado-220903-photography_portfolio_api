f2fa1e8298c98813a518610aabe7f39f76074fa3
Robert Vari <mail.robertvari@gmail.com>
2022-12-10 14:24:08 +0100

    Django cleanup

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-
-from .models import SiteInfo, About
-
-admin.site.register(SiteInfo)
-admin.site.register(About)
--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
 
-from .views import home_view, contact_view, about_view, gallery_view
-
 urlpatterns = [
-    path("", home_view),
-    path("about/", about_view),
-    path("contact/", contact_view),
-    path("gallery/", gallery_view),
+
 ]
